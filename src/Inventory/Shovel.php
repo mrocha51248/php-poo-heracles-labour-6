1fe2ac0c420cdd6f1e2aad053532e36086c67473
Mario Rocha <mrocha51248@gmail.com>
2021-11-08 16:59:03 +0100

    Create Shovel class

---
+++ src/Inventory/Shovel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Inventory;
+
+use App\Equipable;
+
+class Shovel implements Equipable
+{
+    private string $image = 'shield.svg';
+
+    /**
+     * Get the value of image
+     *
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @param string $image
+     *
+     * @return self
+     */
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
