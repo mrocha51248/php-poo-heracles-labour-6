e3529f9aa30d269c87f6c150e637fd3a82b5252c
Mario Rocha <mrocha51248@gmail.com>
2021-11-08 17:08:46 +0100

    Grass: add digged property

--- src/Tile/Grass.php
+++ src/Tile/Grass.php
@@ -4,5 +4,30 @@ namespace App\Tile;
 
 class Grass extends Tile
 {
-    protected string $image = 'grass.png';
+    protected string $image = 'grass.png';  
+    protected bool $digged = false;
+
+    /**
+     * Get the value of digged
+     *
+     * @return bool
+     */
+    public function isDigged(): bool
+    {
+        return $this->digged;
+    }
+
+    /**
+     * Set the value of digged
+     *
+     * @param bool $digged
+     *
+     * @return self
+     */
+    public function setDigged(bool $digged): self
+    {
+        $this->digged = $digged;
+
+        return $this;
+    }
 }
